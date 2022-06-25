30b629ca03b268958f4f5f0ac33ba32dcb560666
Sergey <dj.flyagin@gmail.com>
2022-06-25 14:03:12 +0300

    Beaute 1_1

--- 1_Lesson/1_1/Program.cs
+++ 1_Lesson/1_1/Program.cs
@@ -1,16 +1,18 @@
 ﻿//string num_str = Console.ReadLine();
 //int num = int.Parse(num_str);
 
+Console.WriteLine("Введи первое число");
 int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введи второе число");
 int num2 = int.Parse(Console.ReadLine());
 
 if (num2 == num1 * num1)
 {
-    Console.WriteLine("да");
+    Console.WriteLine($"{num1}^2 = {num1 * num1} - Да, второе число является квадратом первого!");
 }
 else
 {
-    Console.WriteLine("нет");
+    Console.WriteLine($"{num1}^2 = {num1 * num1} - Нет, второе число не является квадратом первого!");
 }
 
 
