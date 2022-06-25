2af9243c4d6cf762e497e21568f7c50d66e98f82
Sergey <dj.flyagin@gmail.com>
2022-06-25 14:25:37 +0300

    Beauti 1_3

--- 1_Lesson/1_3/Program.cs
+++ 1_Lesson/1_3/Program.cs
@@ -1,4 +1,5 @@
-﻿int x = int.Parse(Console.ReadLine());
+﻿Console.WriteLine("Решаем линейное уравнение. Введи число - X, чтобы узнать число - Y");
+int x = int.Parse(Console.ReadLine());
 int y = 2 * x * x - 4 * x + 6;
-Console.WriteLine(y);
+Console.WriteLine($"y = 2 * {x} * {x} - 4 * {x} + 6");
 
