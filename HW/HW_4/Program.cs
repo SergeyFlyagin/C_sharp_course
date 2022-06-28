e492d93d93664d91ae6004c64bd798b3dfcf0227
Sergey <dj.flyagin@gmail.com>
2022-06-28 15:01:47 +0300

    HW_4

--- HW/HW_4/Program.cs
+++ HW/HW_4/Program.cs
@@ -1,3 +1,10 @@
 ﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
-
+Console.WriteLine("Введи число");
+int x = int.Parse(Console.ReadLine());
+int i = 2;
+while (i <= x) ;
+{
+    Console.Write($"{i} ");
+    i += 2;
+}
