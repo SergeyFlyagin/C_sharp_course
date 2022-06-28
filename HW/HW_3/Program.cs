ec09d01811a1c4ae902fc76c248564739482aa34
Sergey <dj.flyagin@gmail.com>
2022-06-28 14:28:10 +0300

    HW_3

--- HW/HW_3/Program.cs
+++ HW/HW_3/Program.cs
@@ -1,16 +1,14 @@
 ﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
-using System;
-  class Program
-  {
-    static void Main()
-     {
-       Console.Write("Введите целое число ");
-       int x = int.Parse(Console.ReadLine());
-       double x_2 = x % 2;
-       if (x_2==0) 
-          Console.WriteLine("число четное");
-       else
-       Console.WriteLine("Число нечетное");
-      }
-   }
+Console.Write("Введите целое число ");
+int x = int.Parse(Console.ReadLine());
+
+if (x % 2 == 0)
+{
+    Console.WriteLine("число четное");
+}
+else
+{
+Console.WriteLine("Число нечетное");
+}
+
