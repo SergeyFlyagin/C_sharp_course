aa0559e91fa2731512d42b5508d833645a6e3195
Sergey <dj.flyagin@gmail.com>
2022-06-28 13:50:30 +0300

    HW_1

---
+++ 2_Lesson/Program.cs
@@ -0,0 +1,19 @@
+﻿// 4. Напишите программу, которая принимает на вход число и проверяет,
+//    кратно ли оно одновременно 7 и 23.
+
+
+
+void OneOfNum(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        Console.WriteLine("да");
+    
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+
+OneOfNum(16);
+OneOfNum(161);
