1fd72037ef42b3981bc716549cb8a718331e8b80
Sergey <dj.flyagin@gmail.com>
2022-06-28 17:04:24 +0300

    HW_starTwo

---
+++ HW/HW_starTwo/Program.cs
@@ -0,0 +1,6 @@
+﻿// программу, которая на вход принимает значение, а на выходе показывает обратное значение
+
+
+Console.WriteLine("Введи число");
+float x = float.Parse(Console.ReadLine());
+Console.WriteLine($"{1 / x}-> {x}");
