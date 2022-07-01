4512c0534a17ce2a7a872b7fd467c7adcab59f43
Sergey <dj.flyagin@gmail.com>
2022-07-01 16:54:36 +0300

    2_Lesson

---
+++ 2_Lesson/2_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу которая на вход принимает трехзначное число
+// и на выходе показывает последнюю цифру
+
+int TakeNum(int num)
+{
+    return num % 10;
+}
+
+Console.WriteLine(TakeNum(678));
+Console.WriteLine(TakeNum(678222));
---
+++ 2_Lesson/2_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// 2. Напишите программу, которая выводит случайное число
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int MaxNumber(int num)
+{
+    if (num % 10 > num / 10)
+    {
+        return num % 10;
+    }
+    else
+        return num / 10;
+}
+int SecondNum = new Random().Next(10, 99);
+MaxNumber(SecondNum);
+Console.WriteLine(SecondNum);
+Console.WriteLine(MaxNumber(SecondNum));
