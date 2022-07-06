6bfe86ce3f002f18746de25e8bf0f0bff594263d
Sergey <dj.flyagin@gmail.com>
2022-07-06 11:54:48 +0300

    HW_4

---
+++ HW_4/4_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumNum(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+Console.WriteLine(SumNum(7836));
+
---
+++ HW_4/4_3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] arr = { 1, 5, 4, 3, 2, 8, 7, 1,};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(arr);
