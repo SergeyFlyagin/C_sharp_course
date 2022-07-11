c6f56cd89fbda82034abcdf37b08b54d7a1ba89c
Sergey <dj.flyagin@gmail.com>
2022-07-11 16:01:30 +0300

    5_Lesson

---
+++ 5_Lesson/5_3/Program.cs
@@ -0,0 +1,40 @@
+﻿// // 2. Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(-9, 10);
+    }
+    return arr;
+}
+
+void MassNum(int[] arr, int num)
+{
+    string num_1 = "Нет";
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (num == arr[i])
+        {
+            num_1 = "Да";
+            break;
+        }
+    }
+    Console.WriteLine($"{num}, {num_1} ");
+}
+int[] arr_1 = MassNums(12);
+Print(arr_1);
+MassNum(arr_1, 4);
+
---
+++ 5_Lesson/5_4/Program.cs
@@ -0,0 +1,38 @@
+﻿// // 3. Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(1, 500);
+    }
+    return arr;
+}
+
+void FindNum(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] < 100)
+        {
+            count ++;
+        }
+    }
+    Console.WriteLine($"-> {count} ");
+}
+int[] arr_1 = MassNums(123);
+Print(arr_1);
+FindNum(arr_1);
