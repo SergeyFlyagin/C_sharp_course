8a5bf21d849e217c7431294f6dda590d4a6f0d04
Sergey <dj.flyagin@gmail.com>
2022-07-12 08:47:13 +0300

    hw_5

--- 5_Lesson/HW_5/5_1/Program.cs
+++ 5_Lesson/HW_5/5_1/Program.cs
@@ -19,7 +19,7 @@ int[] MassNums(int size)
     int[] arr = new int[size];
     for (int i = 0; i < size; i++)
     {
-        arr[i] = new Random().Next(1, 500);
+        arr[i] = new Random().Next(100, 1000);
     }
     return arr;
 }
@@ -29,13 +29,13 @@ void FindNum(int[] arr)
     int count = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i] >= 10 && arr[i] < 100)
+        if (arr[i] % 2 == 0)
         {
-            count ++;
+            count++;
         }
     }
     Console.WriteLine($"-> {count} ");
 }
-int[] arr_1 = MassNums(123);
+int[] arr_1 = MassNums(6);
 Print(arr_1);
 FindNum(arr_1);
