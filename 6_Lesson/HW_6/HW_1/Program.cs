74f0508b8a3033aa7fed90691df78b43241ddaa1
Sergey <dj.flyagin@gmail.com>
2022-07-17 10:30:15 +0300

    HW_6

---
+++ 6_Lesson/HW_6/HW_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+ Console.Write("Введите элементы, через пробел. После ввода, нажмите Enter: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Кол-во элементов > 0: {count}");
+
---
+++ 6_Lesson/HW_6/HW_3/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая будет создавать копию
+//  заданного двумерного массива с помощью поэлементного копирования.
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void CopyArray(int[,] matr)
+{
+    int row = matr.GetLength(0);
+    int column = matr.GetLength(1);
+    int[,] new_matr = new int[row, column];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            new_matr[i, j] = matr[i, j];
+        }
+    }
+}
+int[,] matrix = new int[3, 4];
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+Console.WriteLine();
+int[,] new_matrix = new int[3, 4];
+CopyArray(new_matrix);
